61d7227a4290f3b37b8a3710ed0c723732cbcfc5
Miguel Matul <miguemat4@gmail.com>
2022-03-29 11:40:32 -0600

    Uso de semáforos como solución

--- Thread Race/src/main/frmMain.java
+++ Thread Race/src/main/frmMain.java
@@ -5,17 +5,72 @@
  */
 package main;
 
+import java.util.concurrent.Semaphore;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JLabel;
+
 /**
  *
  * @author migu_
  */
 public class frmMain extends javax.swing.JFrame {
+    
+    int contador = 1;
+    // Procesos
+    Proceso hilo1;
+    Proceso hilo2;
+    Proceso hilo3;
+    // Región Crítica
+    int[] regionCritica = new int[3];
+    int posicion = 0;
+    // Semáforo para exclusión mutua
+    private static Semaphore mutex = new Semaphore(1, true); // Controla el acceso a la región crítica
 
     /**
      * Creates new form frmMain
      */
     public frmMain() {
         initComponents();
+        this.hilo1 = new Proceso(lblNumeroHilo1);
+        this.hilo2 = new Proceso(lblNumeroHilo2);
+        this.hilo3 = new Proceso(lblNumeroHilo3);
+        regionCritica[0] = 0;
+        regionCritica[1] = 0;
+        regionCritica[2] = 0;
+    }
+    
+    public class Proceso extends Thread {
+        int numeroAGenerar;
+        JLabel miEtiqueta;
+        String status;
+
+        public Proceso(JLabel etiqueta) {
+            numeroAGenerar = 0;
+            this.miEtiqueta = etiqueta;
+        }
+        
+        @Override
+        public void run() {
+            // Operaciones pre región crítica
+            this.numeroAGenerar = (int)(Math.random()* 9 + 1);
+            this.miEtiqueta.setText(String.valueOf(numeroAGenerar));
+            // Región crítica
+            try {
+                mutex.acquire();
+            } catch (InterruptedException ex) {
+                Logger.getLogger(frmMain.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            regionCritica[posicion] = this.numeroAGenerar;
+            String contenidoRC = "[" + String.valueOf(regionCritica[0]) + "]";
+            contenidoRC += "[" + String.valueOf(regionCritica[1]) + "]";
+            contenidoRC += "[" + String.valueOf(regionCritica[2]) + "]";
+            lblRegionCritica.setText(contenidoRC);
+            posicion++;
+            mutex.release(); // Libera región crítica
+            // Operaciones post región crítica
+            System.out.println("Proceso finalizado con status: 0");
+        }
     }
 
     /**
@@ -38,11 +93,13 @@ public class frmMain extends javax.swing.JFrame {
         lblStatus2 = new javax.swing.JLabel();
         lblStatus3 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
+        btnIniciar = new javax.swing.JButton();
+        btnPosicion = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         lblRegionCritica.setFont(new java.awt.Font("Tahoma", 1, 48)); // NOI18N
-        lblRegionCritica.setText("[  ]");
+        lblRegionCritica.setText("[0][0][0]");
 
         jLabel1.setText("Hilo 1");
 
@@ -67,45 +124,72 @@ public class frmMain extends javax.swing.JFrame {
 
         jLabel4.setText("Región crítica");
 
+        btnIniciar.setText("Iniciar");
+        btnIniciar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnIniciarActionPerformed(evt);
+            }
+        });
+
+        btnPosicion.setText("Posición");
+        btnPosicion.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnPosicionActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(45, 45, 45)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(lblNumeroHilo1)
-                    .addComponent(jLabel1))
-                .addGap(114, 114, 114)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(lblNumeroHilo2)
-                    .addComponent(jLabel2))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel3, javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(lblNumeroHilo3, javax.swing.GroupLayout.Alignment.TRAILING))
-                .addGap(46, 46, 46))
-            .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(160, 160, 160)
-                        .addComponent(lblRegionCritica))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addGap(31, 31, 31)
-                        .addComponent(lblStatus1)
+                        .addGap(127, 127, 127)
+                        .addComponent(lblRegionCritica)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(jLabel4)
-                            .addComponent(lblStatus2))))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 80, Short.MAX_VALUE)
-                .addComponent(lblStatus3)
-                .addGap(33, 33, 33))
+                        .addComponent(btnPosicion))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(45, 45, 45)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                    .addComponent(lblNumeroHilo1)
+                                    .addComponent(jLabel1))
+                                .addGap(133, 133, 133)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                    .addComponent(lblNumeroHilo2)
+                                    .addComponent(jLabel2))
+                                .addGap(151, 151, 151)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(jLabel3)
+                                    .addGroup(layout.createSequentialGroup()
+                                        .addGap(6, 6, 6)
+                                        .addComponent(lblNumeroHilo3))))
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(203, 203, 203)
+                                .addComponent(jLabel4)))
+                        .addGap(0, 0, Short.MAX_VALUE)))
+                .addContainerGap())
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(31, Short.MAX_VALUE)
+                .addComponent(lblStatus1)
+                .addGap(102, 102, 102)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(btnIniciar)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(lblStatus2)
+                        .addGap(122, 122, 122)
+                        .addComponent(lblStatus3)))
+                .addGap(47, 47, 47))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(lblRegionCritica)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(lblRegionCritica)
+                    .addComponent(btnPosicion))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jLabel4)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 68, Short.MAX_VALUE)
@@ -123,11 +207,30 @@ public class frmMain extends javax.swing.JFrame {
                     .addComponent(lblStatus1)
                     .addComponent(lblStatus2)
                     .addComponent(lblStatus3))
-                .addGap(57, 57, 57))
+                .addGap(18, 18, 18)
+                .addComponent(btnIniciar)
+                .addGap(17, 17, 17))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void btnIniciarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnIniciarActionPerformed
+        hilo1.start();
+        hilo2.start();
+        hilo3.start();
+        /*if (contador == 1)
+            hilo1.start();
+        if (contador == 2)
+            hilo2.start();
+        if (contador == 3)
+            hilo3.start();*/
+        contador++;
+    }//GEN-LAST:event_btnIniciarActionPerformed
+
+    private void btnPosicionActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnPosicionActionPerformed
+        System.out.println("La posición actual libre para Región crítica es: " + posicion);
+    }//GEN-LAST:event_btnPosicionActionPerformed
 
     /**
      * @param args the command line arguments
@@ -165,6 +268,8 @@ public class frmMain extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnIniciar;
+    private javax.swing.JButton btnPosicion;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
